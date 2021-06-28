80917944d9bc801bc5897332de2f03b9ce7c46ab
E-Burton <edwina.antoinette@gmail.com>
2021-06-28 14:28:22 -0600

    Moved User Interface Pt. 2 code within while loop. Modified if conditional to evaluate user input. Updated while loop if conditionals for instructions to evaluate if backPages and nextPages stacks are not empty.

--- script.js
+++ script.js
@@ -5,30 +5,34 @@ const prompt = require('prompt-sync')();
 // ------------------------------
 const backPages = new Stack();
 const nextPages = new Stack();
-let currentPage = null;
+let currentPage = "Starting Page";
 // ------------------------------
 // Helper Functions
 // ------------------------------
 function showCurrentPage(action) {
-  console.log(`User input: ${action} \n Current Page: ${currentPage} \n Top Back Page: ${backPages.head} \n Top Next Page: ${nextPages.head}`);
+  console.log(`User input: ${action} \n Current Page: ${currentPage} \n Top Back Page: ${backPages.peek()} \n Top Next Page: ${nextPages.peek()}`);
 }
 
 function newPage(page) {
+  console.log(`I'm in the new page function!`)
   backPages.push(currentPage);
   currentPage = page;
-  while (nextPages !== null) {
+  while (nextPages.size > 0) {
+    console.log(`I'm in the while function!`);
     nextPages.pop();
   }
-  showCurrentPage();
+  showCurrentPage('New Page');
 }
 
 function backPage() {
-  nextPage.push(currentPage);
+  console.log(`I'm in the back pages function!`)
+  nextPages.push(currentPage);
   currentPage = backPages.pop();
   showCurrentPage('Back');
 }
 
 function nextPage() {
+  console.log(`I'm in the next pages function!`)
   backPages.push(currentPage);
   currentPage = nextPages.pop();
   showCurrentPage('Next')
@@ -49,17 +53,15 @@ let finish = false;
 let showBack = false;
 let showNext = false;
 
-showCurrentPage('Start Page');
-
 while (!finish) {
-  const instructions = baseInfo;
-  if (!backPages.isEmpty()) {
+  let instructions = baseInfo;
+  if (backPages.size > 0) {
     instructions += `, ${backInfo}`;
     showBack = true;
   } else {
     showBack = false;
   }
-  if (!nextPages.isEmpty()) {
+  if (nextPages.size > 0) {
     instructions += `, ${nextInfo}`
     showNext = true;
   } else {
@@ -68,27 +70,30 @@ while (!finish) {
   
   instructions += `, ${quitInfo}`;
   console.log(instructions);
-  }
+
   // ------------------------------
   // User Interface Part 2
   // ------------------------------
   const answer = prompt(question);
-  const lowerCaseAnswer = answer.toLower();
+  const lowerCaseAnswer = answer.toLowerCase();
   
-  if(lowerCaseAnswer !== b || lowerCaseAnswer !== n || lowerCaseAnswer !== q) {
+  if(lowerCaseAnswer !== 'b' && lowerCaseAnswer !== 'n' && lowerCaseAnswer !== 'q') {
     newPage(answer);
-  } else if (lowerCaseAnswer === b) {
+  } else if (lowerCaseAnswer === 'b') {
     if (showBack) {
       backPage();
     } else {
       console.log("Sorry, there is no previous page to visit.");
     }
-  } else if (lowerCaseAnswer === n) {
+  } else if (lowerCaseAnswer === 'n') {
     if (showNext) {
       nextPage();
     } else {
       console.log("Sorry, there is no next page to visit.")
     }
-  } else if (lowerCaseAnswer === q) {
+  } else if (lowerCaseAnswer === 'q') {
     finish = true;
   }
+}
+  
+  
